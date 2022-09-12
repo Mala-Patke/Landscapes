e7894711496ed85f084c9e10be22dc4135524e31
Mala Patke <alis@khanlabschool.org>
2022-09-11 18:55:39 -0700

    Initial Commit

---
+++ Landscapes/LandscapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandscapesApp.swift
+//  Landscapes
+//
+//  Created by Ali Shahid on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LandscapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
